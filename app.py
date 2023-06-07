a4b5ad57ffff74621f4b6b66a0a525bfbb471e8b
Nicola Croce <nicox@Nicolas-MacBook-Air.local>
2023-06-07 11:46:46 +0100

    backend almost done, throws an error

---
+++ app.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title='pdf Oracle')
+    st.header('Chat with PDF Oracle')
+
+    #uploading PDF File
+    pdf = st.file_uploader("Upload your PDF Document", type='pdf')
+
+    #extract text in the PDF file
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        
+        #split the text extracted from PDF into chunks
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200, 
+            length_function=len
+        )
+
+        chunks = text_splitter.split_text(text)
+
+        #create embeddings
+        embeddings = OpenAIEmbeddings()
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+        st.write(knowledge_base)
+
+
+
+if __name__ == '__main__':
+    main()
